220753dc7d1be8d5624765b89652ee90d062095b
student-30 class-2 <class-2s30@jhu.edu@login-node03.cm.cluster>
2017-01-13 15:05:00 -0500

    quadratic plotting functions

---
+++ function.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+def quadratic(a, b, c):
+	n = 10
+	beg = -1.
+	end = 1.
+	delta = (end - beg) / (n - 1)
+
+	x = np.zeros(n)
+	y = np.zeros(n)
+
+	for i in range(n):
+		x[i] = beg + i * delta
+		y[i] = a*x[i]**2 + b*x[i] + c
+	return(x,y)
--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python
 
 import readwrite
+import function
+import plot
+
 [a,b,c]=readwrite.read()
+
 print(a,b,c)
 
+[x,y] = function.quadratic(a,b,c)
+
+print(x,y)
+
+plot.plot(x,y)
